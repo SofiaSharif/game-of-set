32f232ba26cd2b2c90cf91a790228e6c2fc7c066
ssharif3 <174224547+SofiaSharif@users.noreply.github.com>
2025-11-23 19:02:53 -0600

    Added react draggable for floating windows

--- gameofset/src/components/GamePanel.jsx
+++ gameofset/src/components/GamePanel.jsx
@@ -1,25 +1,30 @@
-import Card from './Card'
+import Card from './Card';
 import Header from './Header';
+import Draggable from 'react-draggable';
+import { useRef } from 'react';
 
 function GamePanel({cards, isMinimized, onMinimize, onClose}) {
     const style = {display: isMinimized ? "none" : "grid"};
+    const nodeRef = useRef(null);
 
     return(
         <>
-        <div className="game-panel">
-            <Header isMinimized={isMinimized} title="Cards" onMinimize={() => onMinimize("Game Panel")} onClose={() => onClose("Game Panel")}/>
-            <div id="cards" className="cards" style={style}>
-                {cards.map((card) => (
-                    <Card 
-                        key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
-                        num={card.num} 
-                        color={card.color} 
-                        shape={card.shape} 
-                        bgd={card.bgd}
-                    />
-                ))}
+        <Draggable nodeRef={nodeRef} handle=".header">
+            <div ref={nodeRef} className="game-panel">
+                <Header isMinimized={isMinimized} title="Cards" onMinimize={() => onMinimize("Game Panel")} onClose={() => onClose("Game Panel")}/>
+                <div id="cards" className="cards" style={style}>
+                    {cards.map((card) => (
+                        <Card 
+                            key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
+                            num={card.num} 
+                            color={card.color} 
+                            shape={card.shape} 
+                            bgd={card.bgd}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>
+        </Draggable>
         </>
     );
 }
--- gameofset/src/components/Solutions.jsx
+++ gameofset/src/components/Solutions.jsx
@@ -1,28 +1,31 @@
 import Card from './Card'
 import Header from './Header';
+import Draggable from 'react-draggable';
+import { useRef } from 'react';
 
 function Solutions({solutions, isMinimized, onMinimize, onClose}) {
     const style = {display: isMinimized ? "none" : "grid"};
+    const nodeRef = useRef(null);
 
     return(
-        <>
-        <div className="solutions-panel">
-            <Header isMinimized={isMinimized} title="Solutions" onMinimize={() => onMinimize("Solutions Panel")} onClose={() => onClose("Solutions Panel")}/>
-            <div className='cards' style={style}>
-                {solutions.map((sol) => (
-                    sol.map((card) => (
-                        <Card 
-                            key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
-                            num={card.num} 
-                            color={card.color} 
-                            shape={card.shape} 
-                            bgd={card.bgd}
-                        />
-                    ))
-                ))}
+        <Draggable nodeRef={nodeRef} handle=".header">
+            <div ref={nodeRef} className="solutions-panel">
+                <Header isMinimized={isMinimized} title="Solutions" onMinimize={() => onMinimize("Solutions Panel")} onClose={() => onClose("Solutions Panel")}/>
+                <div className='cards' style={style}>
+                    {solutions.map((sol) => (
+                        sol.map((card) => (
+                            <Card 
+                                key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
+                                num={card.num} 
+                                color={card.color} 
+                                shape={card.shape} 
+                                bgd={card.bgd}
+                            />
+                        ))
+                    ))}
+                </div>
             </div>
-        </div>
-        </>
+        </Draggable>
     );
 }
 
