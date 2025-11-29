5cba781f649a75c946f4079ebf7df6192ea09089
ssharif3 <174224547+SofiaSharif@users.noreply.github.com>
2025-11-28 20:04:23 -0600

    Fixed collapsing window behavior

--- gameofset/src/components/Solutions.jsx
+++ gameofset/src/components/Solutions.jsx
@@ -11,20 +11,22 @@ function Solutions({solutions, numChecks, isMinimized, onMinimize, onClose}) {
         <Draggable nodeRef={nodeRef} handle=".header">
             <div ref={nodeRef} className="solutions-panel">
                 <Header isMinimized={isMinimized} title="Solutions" onMinimize={() => onMinimize("Solutions Panel")} onClose={() => onClose("Solutions Panel")}/>
-                <h3>Number of Checks: {numChecks}</h3>
-                <h3>Number of Solutions: {solutions.length}</h3>
-                <div className='cards' style={style}>
-                    {solutions.map((sol) => (
-                        sol.map((card) => (
-                            <Card 
-                                key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
-                                num={card.num} 
-                                color={card.color} 
-                                shape={card.shape} 
-                                bgd={card.bgd}
-                            />
-                        ))
-                    ))}
+                <div className='solutions-container' style={style}>
+                    <h3>Number of Checks: {numChecks}</h3>
+                    <h3>Number of Solutions: {solutions.length}</h3>
+                    <div className='cards'>
+                        {solutions.map((sol) => (
+                            sol.map((card) => (
+                                <Card 
+                                    key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} 
+                                    num={card.num} 
+                                    color={card.color} 
+                                    shape={card.shape} 
+                                    bgd={card.bgd}
+                                />
+                            ))
+                        ))}
+                    </div>
                 </div>
             </div>
         </Draggable>
