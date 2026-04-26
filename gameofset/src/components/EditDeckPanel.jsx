058db8a23f4f433ae13609c46395e38b1435cf28
SofiaSharif <174224547+SofiaSharif@users.noreply.github.com>
2026-04-25 20:11:55 -0500

    Fixed issue with duplicate cards in editor
    If there were duplicates in the editor and the
    "New Game" button was clicked, the duplicate
    cards would stay in the editor. This was due to
    multiple React keys.
    
    The issue was fixed by assigning the key to an
    index instead of the card attributes.

--- gameofset/src/components/EditDeckPanel.jsx
+++ gameofset/src/components/EditDeckPanel.jsx
@@ -39,7 +39,7 @@ function EditDeckPanel({cards, onCardClick, isMinimized, isVisible, onMinimize, 
                     <PanelHeader isMinimized={isMinimized} title="Edit Deck" onMinimize={() => onMinimize("Edit Deck Panel")} onClose={() => onClose("Edit Deck Panel")}/>
                     <div className="deck-grid-editor" style={container_style}>
                         {draftCards.map((card, index) => (
-                            <div key={`${card.num}-${card.color}-${card.shape}-${card.bgd}`} className="card-edit-row">
+                            <div key={`card-${index}`} className="card-edit-row">
                                 <span>Card {index + 1}:</span>
 
                                 <select value={card.num} onChange={(e) => handleSelectChange(index, "num", parseInt(e.target.value))}>
